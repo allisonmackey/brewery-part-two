e2f3805f73ee28c11fc9b2f57d0b6814f72a9f88
Allison Mackey <a.mackey693@gmail.com>
2020-07-10 09:06:23 -0700

    add test for #formVisibleReduer 'should return default state if no action type is recognized' test fails as expected

--- src/__tests__/reducers/form-visible-reducer.test.js
+++ src/__tests__/reducers/form-visible-reducer.test.js
@@ -1,2 +1,8 @@
 import formVisibleReducer from '../../reducers/form-visible-reducer'
 
+describe('formVisibleReducer', () => {
+  test('should return default state if no action type is recognized', () => {
+    expect(formVisibleReducer(false, {type:null})).toEqual('landing-page');
+  });
+
+});
