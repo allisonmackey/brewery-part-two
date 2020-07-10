ff2778c3019b86561753ba18436f45493439bf9b
Allison Mackey <a.mackey693@gmail.com>
2020-07-10 11:08:31 -0700

    refactor #formVisibleReducer tests to include action types, first test PASSES

--- src/__tests__/reducers/form-visible-reducer.test.js
+++ src/__tests__/reducers/form-visible-reducer.test.js
@@ -1,17 +1,18 @@
 import formVisibleReducer from '../../reducers/form-visible-reducer'
+import * as c from '../../actions/ActionTypes';
 
 describe('formVisibleReducer', () => {
   test('should return default state if no action type is recognized', () => {
     expect(formVisibleReducer('landing-page', {type:null})).toEqual('landing-page');
   });
   test('should change form visiblility state to keg-list when action type is equal to KEG_LIST', () => {
-    expect(formVisibleReducer(false, {type:'KEG_LIST'})).toEqual('keg-list');
+    expect(formVisibleReducer(false, {type: c.KEG_LIST} )).toEqual('keg-list');
   });
-  test('should change form visiblility state back to landing-page when action type is equal to LANDING_PAGE', () => {
-    expect(formVisibleReducer(false, {type:'LANDING_PAGE'})).toEqual('landing-page');
-  });    
-  test('should change form visiblility state to add-keg when action type is equal to SEE_FORM', () => {
-    expect(formVisibleReducer(false, {type:'SEE_FORM'})).toEqual('SEE_FORM');
-  });    
+  // test('should change form visiblility state back to landing-page when action type is equal to LANDING_PAGE', () => {
+  //   expect(formVisibleReducer(false, {type:'LANDING_PAGE'})).toEqual('landing-page');
+  // });    
+  // test('should change form visiblility state to add-keg when action type is equal to SEE_FORM', () => {
+  //   expect(formVisibleReducer(false, {type:'SEE_FORM'})).toEqual('SEE_FORM');
+  // });    
   
 });  
--- src/reducers/form-visible-reducer.js
+++ src/reducers/form-visible-reducer.js
@@ -1,12 +1,14 @@
+import * as c from './../actions/ActionTypes';
+
 export default (state = 'landing-page', action) => {
   switch (action.type) {
-    case 'KEG_LIST': 
+    case c.KEG_LIST: 
       state = 'keg-list';
       return state;
-    case 'LANDING_PAGE':
+    case c.LANDING_PAGE:
       state = 'landing-page';
       return state;
-    case 'SEE_FORM':
+    case c.SEE_FORM:
       state = 'see-form'
       return state;
   default: 
