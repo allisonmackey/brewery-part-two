6810f033aa23de46257c72e0dceb33251f6175f5
Allison Mackey <a.mackey693@gmail.com>
2020-07-10 09:13:12 -0700

    refactor first test to align with default state setting 'landing-page'

--- src/__tests__/reducers/form-visible-reducer.test.js
+++ src/__tests__/reducers/form-visible-reducer.test.js
@@ -2,7 +2,10 @@ import formVisibleReducer from '../../reducers/form-visible-reducer'
 
 describe('formVisibleReducer', () => {
   test('should return default state if no action type is recognized', () => {
-    expect(formVisibleReducer(false, {type:null})).toEqual(false);
+    expect(formVisibleReducer('landing-page', {type:null})).toEqual('landing-page');
   });
-
+  test('should change form visiblility state to keg-list when action type is equal to KEG_LIST', () => {
+    expect(formVisibleReducer(false, {type:'KEG_LIST'})).toEqual('keg-list');
+  });
+  
 });
