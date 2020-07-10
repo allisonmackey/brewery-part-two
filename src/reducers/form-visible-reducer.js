85100055b49d517c61498aa7423de6d3b2a9e9d3
Allison Mackey <a.mackey693@gmail.com>
2020-07-10 09:15:31 -0700

    add logic to pass test for #formVisibleReduer 'should change form visiblity state to keg-list when action type is equal to KEG_LIST' test PASSES

--- src/reducers/form-visible-reducer.js
+++ src/reducers/form-visible-reducer.js
@@ -1,3 +1,10 @@
 export default (state = 'landing-page', action) => {
-  return state;
+  switch (action.type) {
+    case 'KEG_LIST': 
+    state = 'keg-list';
+    return state;
+  
+  default: 
+    return state;
+  }
 }
