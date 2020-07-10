5ec35c56c806e06ffc477126c987d3c621c67c34
Allison Mackey <a.mackey693@gmail.com>
2020-07-10 09:40:11 -0700

    add test for #kegListReducer 'should sucessfully add new keg data to kegListReducer' test fails as expected

--- src/__tests__/reducers/keg-list-reducer.test.js
+++ src/__tests__/reducers/keg-list-reducer.test.js
@@ -1,7 +1,41 @@
 import kegListReducer from '../../reducers/keg-list-reducer';
 
 describe('kegListReducer', () => {
+  let action;
+  const kegData = {
+    name: "IPA",
+    brand: "Bouy Beer Co.",
+    price: 150,
+    alcoholContent: 7.5,
+    pintsLeft: 124,
+    id: 3
+  }
+
+  test('should sucessfully add new keg data to kegListReducer', () => {
+    const {name, brand, price, alcoholContent, pintsLeft, id} = kegData;
+    action = {
+      type: 'ADD_KEG',
+      name: name,
+      brand: brand, 
+      price: price,
+      alcoholContent: alcoholContent,
+      pintsLeft: pintsLeft,
+      id: id
+    };
+
+    expect(kegListReducer({}, {action})).toEqual({
+      [id]: {
+        name: name,
+        brand: brand, 
+        price: price,
+        alcoholContent: alcoholContent,
+        pintsLeft: pintsLeft,
+        id: id
+      }
+    });
+  })
   test('should return default state if there is no action type passed into the reducer', () => {
     expect(kegListReducer({}, {type:null})).toEqual({});
   })
+
 })
